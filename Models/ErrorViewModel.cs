3d328710ad941d5f3d044fb89f8c1d4552cb3429
rthokala141 <62602513+rthokala141@users.noreply.github.com>
2022-05-09 10:35:38 +0530

    teja1209

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevOps_Sai.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
